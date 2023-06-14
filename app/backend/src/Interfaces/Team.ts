1994d86a688034ec6e0cca4429eb604ccac69a2f
Matheus Lucas <matheus.lucas1105@gmail.com>
2023-06-14 13:55:36 -0300

    sequelize configurado

---
+++ app/backend/src/Interfaces/Team.ts
@@ -0,0 +1,6 @@
+/* eslint-disable camelcase */
+export default interface Team {
+  id: number,
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  team_name: string,
+}
---
+++ app/backend/src/database/migrations/create team-table.ts
@@ -0,0 +1,22 @@
+import { Model, QueryInterface, DataTypes } from 'sequelize';
+import Team from '../../Interfaces/Team'
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<Team>>('teams', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      team_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    });
+  },
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('teams');
+  },
+};
