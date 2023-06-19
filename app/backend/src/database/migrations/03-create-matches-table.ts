ad7313b0119a95a54a6b69db0300828d5b376153
Matheus Lucas <matheus.lucas1105@gmail.com>
2023-06-19 15:14:38 -0300

    .

--- app/backend/src/database/migrations/03-create-matches-table.ts
+++ app/backend/src/database/migrations/03-create-matches-table.ts
@@ -1,9 +1,9 @@
 import { Model, QueryInterface, DataTypes } from 'sequelize';
-import  IMatches  from '../../Interfaces/IMatch';
+import { IMatch } from '../../Interfaces/IMatch';
 
 export default {
   up(queryInterface: QueryInterface) {
-    return queryInterface.createTable<Model<IMatches>>('matches', {
+    return queryInterface.createTable<Model<IMatch>>('matches', {
       id: {
         type: DataTypes.INTEGER,
         allowNull: false,
@@ -11,41 +11,38 @@ export default {
         primaryKey: true,
       },
       homeTeamId: {
-        field: 'home_team_id',
         type: DataTypes.INTEGER,
         allowNull: false,
+        field: 'home_team_id',
         references: {
           model: 'teams',
           key: 'id',
         },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
       },
       homeTeamGoals: {
-        field: 'home_team_goals',
-        type: DataTypes.INTEGER,
-        allowNull: false,        
-      },
-      awayTeamId: {
-        field: 'away_team_id',
         type: DataTypes.INTEGER,
         allowNull: false,
+        field: 'home_team_goals',
+      },
+      awayTeamId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: 'away_team_id',
         references: {
           model: 'teams',
           key: 'id',
         },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
       },
       awayTeamGoals: {
-        field: 'away_team_goals',
         type: DataTypes.INTEGER,
         allowNull: false,
+        field: 'away_team_goals',
       },
       inProgress: {
-        field: 'in_progress',
         type: DataTypes.BOOLEAN,
         allowNull: false,
+        defaultValue: false,
+        field: 'in_progress',
       },
     });
   },
